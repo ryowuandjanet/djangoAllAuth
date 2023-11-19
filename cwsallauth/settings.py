8c9b395c18e4fc2d2a70341a98a3de07ab436139
ryowuandjanet <ryowu0329@gmail.com>
2023-11-19 20:05:21 +0800

    complete github social account

--- cwsallauth/settings.py
+++ cwsallauth/settings.py
@@ -5,6 +5,13 @@ SECRET_KEY = 'django-insecure-1s*41+xa=p3if@fnmxnia@iag8c3ws&t)m#n(uo0(owj0+nfj^
 DEBUG = True
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+  'django.contrib.auth.backends.ModelBackend',
+  'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 INSTALLED_APPS = [
   'django.contrib.admin',
   'django.contrib.auth',
@@ -12,6 +19,11 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
+  'django.contrib.sites',
+  'allauth',
+  'allauth.account',
+  'allauth.socialaccount',
+  'allauth.socialaccount.providers.github',
 ]
 
 MIDDLEWARE = [
@@ -22,14 +34,17 @@ MIDDLEWARE = [
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
   'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'cwsallauth.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATES = [
   {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [],
+    'DIRS': [ BASE_DIR / 'templates' ],
     'APP_DIRS': True,
     'OPTIONS': {
       'context_processors': [
@@ -37,10 +52,21 @@ TEMPLATES = [
         'django.template.context_processors.request',
         'django.contrib.auth.context_processors.auth',
         'django.contrib.messages.context_processors.messages',
+        'django.template.context_processors.request',
       ],
     },
   },
 ]
+
+SOCIALACCOUNT_PROVIDERS = {
+  'github': {
+    'APP': {
+      'client_id': 'eaff3a7eef5dee5c3277',
+      'secret': 'e829c85b13ab390b0ef7ced4593aecb74f639682',
+      'key': ''
+    }
+  }
+}
 
 WSGI_APPLICATION = 'cwsallauth.wsgi.application'
 
